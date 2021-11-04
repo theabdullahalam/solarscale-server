87a56c7fa22330423e9b69167df36241e5dfc572
Abdullah Alam <abdullahalam@protonmail.com>
2021-11-04 17:17:59 +0530

    add endpoint to fetch data based on object size

--- controllers/planetsController.js
+++ controllers/planetsController.js
@@ -11,7 +11,7 @@ exports.get_default_planets = function(req, res, next) {
             // TEST
             let testobj = {
                 p_title: 'SUN',
-                p_radius: 20
+                p_radius: 30
             }
             let newplanets = system_calculator.getFromSize(testobj, planets)
 
@@ -23,4 +23,31 @@ exports.get_default_planets = function(req, res, next) {
         }
     })   
   
+}
+
+exports.get_from_radius = function(req, res, next){
+
+    Planet.find(function(err, planets){
+        if (err){
+            res.send(err)
+        }else{
+            
+            // GET PLANET NAME
+            let title = req.query.object
+            let radius = req.query.radius
+
+            let obj = {
+                p_title: title.toUpperCase(),
+                p_radius: Number(radius)
+            }
+            let newplanets = system_calculator.getFromSize(obj, planets)
+
+            let responsedata = {
+                unit: 'CM',
+                bodies: newplanets
+            }
+            res.json(responsedata);
+        }
+    })
+
 }
--- routes/planets.js
+++ routes/planets.js
@@ -8,4 +8,7 @@ var planets_controller = require('../controllers/planetsController')
 /* GET DEFAULT PLANETS */
 router.get('/defaultplanets', cors(), planets_controller.get_default_planets);
 
+/* GET PLANETS BASED ON SIZE */
+router.get('/getfromsize', cors(), planets_controller.get_from_radius);
+
 module.exports = router;
